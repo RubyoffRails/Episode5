ed1ea36274e3448944436457088a656dbc591d95
Ralphos <wintle.ralph@gmail.com>
2012-05-16 21:45:01 -0500

    Added pages to seed file

--- adventure.rb
+++ adventure.rb
@@ -4,20 +4,9 @@ require 'bundler/setup'
 require_relative 'db/setup'
 require_relative 'models/page'
 require_relative 'models/book'
+require_relative 'db/seed' # lesson learned: this has to be placed below the page/book models. 
 
-page = Page.create(starting_point: true, content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
-Page.create(conclusion: true, 
-            parent_id: page.id, 
-            content: "Go into the forest",
-            preview: "It's dark... you have half a bacon sandwich left... and you need to pee, but you keep only to discover.",
-            winner: false)
-
-Page.create(conclusion: true,
-            parent_id: page.id,
-            content: "Walk down the road",
-            preview: "It's cold... you have no sandwich left... but you see a light and discover...",
-            winner: true)
-
+page = Page.all.first # shouldn't I be able to leave this out and let the page variable be set in the seed file?
 book = Book.new(page)
 
 until book.complete_game? do
