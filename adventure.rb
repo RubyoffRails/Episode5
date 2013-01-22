7ef503b327a983d2fa76299c63893fe652d11d84
Elle Yoko Suzuki <elle.suzuki@gmail.com>
2013-01-21 22:43:17 -0800

    pages now have preview method; clearer outcome

--- adventure.rb
+++ adventure.rb
@@ -6,7 +6,7 @@ require_relative 'models/page'
 require_relative 'models/book'
 
 page = Page.create(starting_point: true, content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
-Page.create(conclusion: true, parent_id: page.id, content: "Go into the forest")
+Page.create(conclusion: true, parent_id: page.id, content: "Go into the forest", win: true)
 Page.create(conclusion: true, parent_id: page.id, content: "Walk down the road")
 
 book = Book.new(page)
@@ -14,22 +14,19 @@ book = Book.new(page)
 until book.complete_game? do
 	puts book.current_page.content
 	puts "your options: "
-	puts "  - [#{book.current_page.options.first.content}]"
-	puts "  - [#{book.current_page.options.last.content}]"
+	puts " Option A: [#{book.current_page.options.first.content}]"
+	puts " Option B: [#{book.current_page.options.last.content}]"
 	puts "What do you want to do? Enter A or B"
 	
 	book.input( gets )
-
+  puts
+  puts book.current_page.preview
+  if book.current_page.win
+    puts 'You Win! You find the Fountain of Youth!'
+  else
+    puts 'You Lose! You are robbed and killed by evil ninjas..'
+  end
 end
-puts "------------------------------------------"
-puts "|                                        |"
-puts "|                                        |"
-puts "|          ADVENTURE COMPLETE            |"
-puts "|                                        |"
-puts "|                                        |"
-puts "------------------------------------------"
 
-
-puts book.current_page.content	
-
-puts "hope you won!"
+puts
+puts " The End! "
