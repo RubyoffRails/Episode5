e9b1d48f6cad9c2eca256fbeffc0a8565227ddca
meltar <meltar@gmail.com>
2013-03-31 17:58:26 -0400

    Panda level

--- adventure.rb
+++ adventure.rb
@@ -5,22 +5,28 @@ require_relative 'db/setup'
 require_relative 'models/page'
 require_relative 'models/book'
 
-page = Page.create(starting_point: true, content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
-Page.create(conclusion: true, parent_id: page.id, content: "Go into the forest")
-Page.create(conclusion: true, parent_id: page.id, content: "Walk down the road")
+page = Page.create(starting_point: true, preview: "Welcome, adventurer.", 
+	content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
+Page.create(conclusion: true, parent_id: page.id, preview: "Go into the forest", 
+	content: "You are attacked by a hungry bear who wants your sandwich.  You lose.")
+Page.create(conclusion: true, parent_id: page.id, preview: "Walk down the road",
+	content: "You made it back to the inn safely.  You win 100 gold pieces!!")
 
 book = Book.new(page)
 
 until book.complete_game? do
 	puts book.current_page.content
 	puts "your options: "
-	puts "  - [#{book.current_page.options.first.content}]"
-	puts "  - [#{book.current_page.options.last.content}]"
+	puts "  - [#{book.current_page.options.first.preview}]"
+	puts "  - [#{book.current_page.options.last.preview}]"
 	puts "What do you want to do? Enter A or B"
 	
 	book.input( gets )
 
 end
+puts book.current_page.content	
+
+puts book.current_page.conclusion
 puts "------------------------------------------"
 puts "|                                        |"
 puts "|                                        |"
@@ -29,7 +35,5 @@ puts "|                                        |"
 puts "|                                        |"
 puts "------------------------------------------"
 
-
-puts book.current_page.content	
 
 puts "hope you won!"
--- db/migrate/001_creates_page.rb
+++ db/migrate/001_creates_page.rb
@@ -2,6 +2,7 @@ class CreatesPage < ActiveRecord::Migration
 	def change
 		create_table :pages do |t|
 			t.text :content
+			t.text :preview
 			t.integer :parent_id
 			t.boolean :starting_point, default: false
 			t.boolean :conclusion, default: false
