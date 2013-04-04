11c1395ed0adf9162448e04a4587bbe87b5bacf3
meltar <meltar@gmail.com>
2013-04-04 19:58:07 -0400

    tiger

--- adventure.rb
+++ adventure.rb
@@ -4,15 +4,9 @@ require 'bundler/setup'
 require_relative 'db/setup'
 require_relative 'models/page'
 require_relative 'models/book'
+require "./db/seed"
 
-page = Page.create(starting_point: true, preview: "Welcome, adventurer.", 
-	content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
-Page.create(conclusion: true, parent_id: page.id, preview: "Go into the forest", 
-	content: "You are attacked by a hungry bear who wants your sandwich.  You lose.")
-Page.create(conclusion: true, parent_id: page.id, preview: "Walk down the road",
-	content: "You made it back to the inn safely.  You win 100 gold pieces!!")
-
-book = Book.new(page)
+book = Book.new(Page.starting_point)
 
 until book.complete_game? do
 	puts book.current_page.content
@@ -21,12 +15,11 @@ until book.complete_game? do
 	puts "  - [#{book.current_page.options.last.preview}]"
 	puts "What do you want to do? Enter A or B"
 	
-	book.input( gets )
+	book.input( gets.chomp().capitalize )
 
 end
 puts book.current_page.content	
 
-puts book.current_page.conclusion
 puts "------------------------------------------"
 puts "|                                        |"
 puts "|                                        |"
--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,7 @@
-# Cleaning Out
+Page.delete_all
+start = Page.create(starting_point: true, preview: "Welcome, adventurer.", 
+	content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
+Page.create(conclusion: true, parent_id: start.id, preview: "Sit down to eat your sandwich", 
+	content: "You are attacked by a hungry bear who smells bacon.  You lose.")
+Page.create(conclusion: true, parent_id: start.id, preview: "Keep walking",
+	content: "You made it back to the inn safely.  You win 100 gold pieces!!")
